79968a7b181fd8519aa65396a8beb4e64daa4a1a
yudhifadilah <115543524+yudhifadilah@users.noreply.github.com>
2024-03-17 12:52:18 +0700

    Update pokemonRoutes.js

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import multer from '../config/MutlerConfig.js'; // Adjust the path to your multer configuration file
+import multer from '../config/MutlerConfig.js'; 
 import * as PokemonController from '../controllers/PokemonController.js';
 
 const router = express.Router();
@@ -7,18 +7,15 @@ const router = express.Router();
 router.get('/pokemons', PokemonController.getAllPokemons);
 router.get('/pokemons/:id', PokemonController.getPokemonById);
 
-// Use multer middleware here
 router.post('/pokemons', (req, res, next) => {
   multer(req, res, (err) => {
     if (err) {
-      // Handle errors caused by multer
       if (err instanceof multer.MulterError) {
         res.status(400).json({ error: err.message });
       } else {
         res.status(500).json({ error: 'Internal Server Error' });
       }
     } else {
-      // Proceed to the createPokemon handler if there are no multer errors
       next();
     }
   });
